2e5858b7a33f0452583a184f8e150693337bc08f
bspiri <54644338+bspiri@users.noreply.github.com>
2020-03-25 11:05:03 +0200

    update Server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((request, response) => {
+  if (request.url == "/goodbye") {
     response.writeHead(200);
-    response.end('Hello World');
+    response.end("Goodbye World");
+  } else if (request.url == "/error") {
+    response.writeHead(500);
+    response.end(JSON.stringify({ error: "server error" }));
+  } else {
+    response.writeHead(200);
+    response.end("Hello World");
+  }
 });
 
 server.listen(3000);
-console.log('Server address: http://localhost:3000');
+console.log("Server address: http://localhost:3000");
